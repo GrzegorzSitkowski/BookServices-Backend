ce39597741b328bd49a23c05ea4b8e497464ea04
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2025-02-10 21:26:38 +0100

    Added new properties in Users.cs

--- BookServices.Domain/Entities/User.cs
+++ BookServices.Domain/Entities/User.cs
@@ -9,8 +9,11 @@ namespace BookServices.Domain.Entities
 {
     public class User : DomainEntity
     {
+        public required string FisrtName { get; set; }
+        public required string LastName { get; set; }
         public required string Email { get; set; }
         public required string HashedPassword { get; set; }
+        public required string Type { get; set; }
         public DateTimeOffset RegisterDate { get; set; }
         public ICollection<AccountUser> AccountUsers { get; set; } = new List<AccountUser>();
     }
