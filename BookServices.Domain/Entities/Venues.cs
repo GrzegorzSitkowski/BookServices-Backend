d794edeef942b7c9b114e3a23a9763c16fb0e58b
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2025-02-12 21:16:10 +0100

    Created entity of Venues.cs

---
+++ BookServices.Domain/Entities/Venues.cs
@@ -0,0 +1,21 @@
+﻿using BookServices.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookServices.Domain.Entities
+{
+    public class Venues : DomainEntity
+    {
+        public required string Name { get; set; }
+        public required string Description { get; set; }
+        public string? PhoneNumber { get; set; }
+        public required string Street { get; set; }
+        public string? PostCode { get; set; }
+        public required string City { get; set; }
+        public DateTimeOffset CreateDate { get; set; } = DateTimeOffset.Now;
+        public int CreatedBy { get; set; }
+    }
+}
