d6871158a07d93f32843ecef32b74a8ffb8fdab2
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2025-02-12 21:18:29 +0100

    Added entity to IApplicationDbContext

--- BookServices.Application/Interfaces/IApplicationDbContext.cs
+++ BookServices.Application/Interfaces/IApplicationDbContext.cs
@@ -11,10 +11,9 @@ namespace BookServices.Application.Interfaces
     public interface IApplicationDbContext
     {
         DbSet<User> Users { get; set; }
-
         DbSet<Account> Accounts { get; set; }
-
         DbSet<AccountUser> AccountUsers { get; set; }
+        DbSet<Venue> Venues { get; set; }
 
         Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
     }
