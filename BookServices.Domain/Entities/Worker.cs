1787f9b89b1aff7fb3c1b212718382efd6d34a84
GrzegorzSitkowski <sitkowskigrzegorz8@gmail.com>
2025-04-08 20:29:57 +0200

    Added entity of Worker.cs

---
+++ BookServices.Domain/Entities/Worker.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BookServices.Domain.Common;
+
+namespace BookServices.Domain.Entities
+{
+    public class Worker : DomainEntity
+    {
+        public int VenueId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int CreatedBy { get; set; }
+        public Venue Venue { get; set; }
+    }
+}
