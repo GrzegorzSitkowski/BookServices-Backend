a61bef7d1a248be626cf4c87c1eded6cc3571346
GrzegorzSitkowski <sitkowskigrzegorz8@gmail.com>
2025-04-09 14:27:33 +0200

    Added DeleteCommand.cs

---
+++ BookServices.Application/Logic/Workers/DeleteCommand.cs
@@ -0,0 +1,54 @@
+﻿using BookServices.Application.Exceptions;
+using BookServices.Application.Interfaces;
+using BookServices.Application.Logic.Abstractions;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookServices.Application.Logic.Workers
+{
+    public class DeleteCommand
+    {
+        public class Request : IRequest<Result>
+        {
+            public int? Id { get; set; }
+        }
+
+        public class Result
+        {
+
+        }
+
+        public class Handler : BaseCommandHandler, IRequestHandler<Request, Result>
+        {
+            public Handler(ICurrentAccountProvider currentAccountProvider, IApplicationDbContext applicationDbContext)
+                : base(currentAccountProvider, applicationDbContext)
+            {
+                    
+            }
+
+            public async Task<Result> Handle (Request request, CancellationToken cancellationToken)
+            {
+                var account = await _currentAccountProvider.GetAuthenticatedAccount();
+
+                var model = await _applicationDbContext.Workers.FirstOrDefaultAsync(v => v.Id == request.Id && v.CreatedBy == account.Id);
+
+                if(model == null)
+                {
+                    throw new UnauthorizedException();
+                }
+
+                _applicationDbContext.Workers.Remove(model);
+
+                await _applicationDbContext.SaveChangesAsync(cancellationToken);
+
+                return new Result();
+            }
+        }
+            
+    }
+}
