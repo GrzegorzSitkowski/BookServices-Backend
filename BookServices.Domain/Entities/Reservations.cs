7baee8a3143449d38a0c01a079a945d0f38e5595
GrzegorzSitkowski <sitkowskigrzegorz8@gmail.com>
2025-04-22 17:09:53 +0200

    Created entity of Reservations

---
+++ BookServices.Domain/Entities/Reservations.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BookServices.Domain.Common;
+
+namespace BookServices.Domain.Entities
+{
+    public class Reservations : DomainEntity
+    {
+        public string Name { get; set; }
+        public string UserName { get; set; }
+        public string WorkerName { get; set; }
+        public string VenueName { get; set; }
+        public int UserId { get; set; }
+        public int VenueId { get; set; }
+        public int WorkerId { get; set; }
+        public DateTimeOffset DateFrom { get; set; }
+        public DateTimeOffset DateTo { get; set; }
+        public Venue Venue { get; set; }
+        public User User { get; set; }
+        public Worker Worker { get; set; }
+    }
+}
